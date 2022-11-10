ff1387d6cd8fbfb7e63fcbd82d3233272dad8969
phamtanphat <phatdroid94@gmail.com>
2022-11-10 19:12:18 +0700

    request

--- lib/common/constants/api_constant.dart
+++ lib/common/constants/api_constant.dart
@@ -1,3 +1,6 @@
 class ApiConstant {
   static final String BASE_URL = "https://serverappsale.herokuapp.com/";
+
+  // End Point
+  static final String SIGN_IN = "user/sign-in";
 }
--- lib/data/datasources/remote/api_request.dart
+++ lib/data/datasources/remote/api_request.dart
@@ -1,4 +1,18 @@
+import 'package:appp_sale_29092022/common/constants/api_constant.dart';
+import 'package:appp_sale_29092022/data/datasources/remote/dio_client.dart';
+import 'package:dio/dio.dart';
 
 class ApiRequest {
+  late Dio dio;
 
+  ApiRequest() {
+    dio = DioClient.instance.dio;
+  }
+
+  Future signInRequest(String email, String password) {
+    return dio.post(ApiConstant.SIGN_IN, data: {
+      "email": email,
+      "password": password
+    });
+  }
 }
