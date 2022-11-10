df347d979c8d431c26cd0e31c1ba22cb5473420b
phamtanphat <phatdroid94@gmail.com>
2022-11-10 18:43:09 +0700

    dto

---
+++ lib/data/datasources/remote/dto/user_dto.dart
@@ -0,0 +1,24 @@
+class UserDTO {
+  String? email;
+  String? name;
+  String? phone;
+  String? token;
+
+  UserDTO({this.email,
+        this.name,
+        this.phone,
+        this.token});
+
+  // Named constructor
+  UserDTO.fromJson(Map<String, dynamic> json) {
+    email = json['email'];
+    name = json['name'];
+    phone = json['phone'];
+    token = json['token'];
+  }
+
+  @override
+  String toString() {
+    return 'UserDTO{email: $email, name: $name, phone: $phone, token: $token}';
+  }
+}
